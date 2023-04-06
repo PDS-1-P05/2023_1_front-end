e5fe03a80bd704c863e2ba4e8727de2ee5344db3
giovanavalli <giovana.valli@ufms.br>
2023-04-06 15:45:03 -0400

    feat: desenvolvimento do footer concluido

--- Regula.sun - front/src/main.js
+++ Regula.sun - front/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import PrimeVue from "primevue/config";
+import Image from 'primevue/image';
 
 import "primevue/resources/themes/lara-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
@@ -11,5 +12,5 @@ const app = createApp(App);
 
 app.use(router);
 app.use(PrimeVue);
-
+app.component('Image', Image);
 app.mount("#app");
