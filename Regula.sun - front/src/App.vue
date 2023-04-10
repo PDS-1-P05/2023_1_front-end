d95d15012929040fc45795271fc81db81cad41c5
leticiayurie <leticia.yurie@ufms.br>
2023-04-10 17:03:14 -0400

    fix: header arrumado para home

--- Regula.sun - front/src/App.vue
+++ Regula.sun - front/src/App.vue
@@ -1,14 +1,23 @@
-<script setup>
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
 </script>
 
 <template>
+  <header>
+    <Header />
+  </header>
   <main>
     <router-view />
   </main>
 </template>
 
 <style>
-
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
@@ -42,7 +51,7 @@
 
   --preto: #231F20;
   --pretoClaro: hsla(345, 6%, 13%, 0.48);
-  
+
   --fontePrincipal: 'Source Sans 3', sans-serif;
   --fonteSecundaria: 'Roboto', sans-serif;
 
@@ -53,5 +62,9 @@ html,
 body {
   width: 100%;
   height: 100%;
+}
+
+main {
+  padding-top: 5rem;
 }
 </style>
