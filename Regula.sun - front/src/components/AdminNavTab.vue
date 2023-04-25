d79daebdf47193616063d39cd4a8890643c22310
Vitor Cassol <vhcassol@gmail.com>
2023-04-25 15:01:47 -0400

    feat: componente AdminNavTab criado

---
+++ Regula.sun - front/src/components/AdminNavTab.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="tab-wrapper">
+        <v-tabs
+            v-model="page"
+            class="tabs"
+            data-test="tabs"
+        >
+            <v-tab value="Indicadores" class="tab" data-test="indicadores">Indicadores</v-tab>
+            <v-tab value="Metas" class="tab" data-test="metas">Metas</v-tab>
+        </v-tabs>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props: {
+            tab: null
+        },
+
+        data() {
+            return {
+                page: null,
+                mySelectedClass: 'my-selected-class'
+            }
+        },
+
+        watch: {
+            page(newPage) {
+                this.$emit('alteraPage', newPage)
+            },
+
+            tab(newTab) {
+                this.page = newTab;
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+.tabs {
+    color: var(--corPrimaria);
+}
+
+.tab .v-btn:hover > .v-btn__overlay {
+    opacity: 0;
+}
+
+
+</style>
