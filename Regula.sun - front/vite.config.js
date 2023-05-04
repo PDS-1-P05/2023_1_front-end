1e229af77c229ffb24b22b524885b56e20ab7797
Vitor Cassol <vhcassol@gmail.com>
2023-05-04 14:49:30 -0400

    chore: mudança do protocolo http para https no dev

--- Regula.sun - front/vite.config.js
+++ Regula.sun - front/vite.config.js
@@ -3,10 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  server: { https: true },
+  plugins: [vue(), vueJsx(), mkcert()],
   test: {
     globals: true,
     environment: 'happy-dom'
