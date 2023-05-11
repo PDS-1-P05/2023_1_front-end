00ce4c8736dae1a8cc6b55fb80c7171a8accd648
leticiayurie <leticia.yurie@ufms.br>
2023-05-11 15:00:40 -0400

    feat: alerta fecha quando clica no x

--- Regula.sun - front/src/router/index.js
+++ Regula.sun - front/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
 import AdminView from "../views/AdminView.vue";
-// import Grafico from "../views/GraficoView.vue";
+import Grafico from "../views/GraficoView.vue";
 // import Boletim from "../views/BoletimView.vue";
 import { validarTokenAcesso } from "../service/autenticacao";
 import store from "../store/index.js";
@@ -15,11 +15,11 @@ path: "/",
 name: "home",
 component: HomeView,
 },
-// {
-//   path: "/grafico",
-//   name: "grafico",
-//   component: Grafico,
-// },
+{
+  path: "/grafico",
+  name: "grafico",
+  component: Grafico,
+},
 // {
 //   path: "/boletim",
 //   name: "boletim",
